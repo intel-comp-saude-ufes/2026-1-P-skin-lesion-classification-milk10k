185cd35c89b42a4ef855bb173d07e58b4b3a7797
gabrieltissianel <gabriel.gtissianel@gmail.com>
2026-07-10 13:22:56 -0300

    tripleblock

--- v4 - metablock/modelo.py
+++ v4 - metablock/modelo.py
@@ -47,7 +47,35 @@ class MILK10kMultimodalModel(nn.Module):
 
         return self.classifier(combined)
 
+class TripleBlock(nn.Module):
+    def __init__(self, num_classes: int, metadata_dim: int):   # MUDANÇA: recebe tb metadata_dim
+        super().__init__()
+
+        backbone = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.num_features = backbone.fc.in_features
+        backbone.fc = nn.Identity()
+        self.backbone = backbone
+
+        self.metablock_derm = MetaBlock(feature_dim=self.num_features, metadata_dim=self.num_features)
+        self.metablock_clin = MetaBlock(feature_dim=self.num_features, metadata_dim=self.num_features)
+
+        combined_dim = self.num_features * 2   # 1024 (derm + clin concatenados)
+        self.metablock_metadata = MetaBlock(feature_dim=combined_dim, metadata_dim=metadata_dim)  # NOVO
+        self.classifier = nn.Linear(combined_dim, num_classes)
+    
+    def forward(self, img_derm, img_clin, metadata):   # MUDANÇA: agora recebe tb metadata
+        feat_derm = self.backbone(img_derm)
+        feat_clin = self.backbone(img_clin)
+
+        feat_metablock_derm = self.metablock_derm(feat_derm, feat_clin)
+        feat_metablock_clin = self.metablock_clin(feat_clin, feat_derm)
+
+        combined = torch.cat([feat_metablock_derm, feat_metablock_clin], dim=1)
+        combined = self.metablock_metadata(combined, metadata)   # NOVO: metadados "filtram" as features de imagem
+
+        return self.classifier(combined)
 
 def criar_modelo(num_classes: int, metadata_dim: int, device) -> nn.Module:   # MUDANÇA
-    model = MILK10kMultimodalModel(num_classes, metadata_dim)
+    #model = MILK10kMultimodalModel(num_classes, metadata_dim)
+    model = TripleBlock(num_classes, metadata_dim)
     return model.to(device)
